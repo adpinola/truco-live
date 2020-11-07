b53775a3ffea4129ed995eec77192aa4acded303
adpinola <alepinola@gmail.com>
2020-11-07 15:33:32 -0300

    fix: run linter and prettier

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { TrucoLive } from './TrucoLive';
-import 'regenerator-runtime/runtime'
+import 'regenerator-runtime/runtime';
 import '@webcomponents/webcomponentsjs/webcomponents-loader';
 
 customElements.define('truco-live', TrucoLive);
