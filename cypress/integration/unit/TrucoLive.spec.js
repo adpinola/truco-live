9e9f12eb52653b005f32d2b89d26ca1b4817ef5f
adpinola <alepinola@gmail.com>
2020-11-07 16:43:34 -0300

    fix: run linter and prettier

--- cypress/integration/unit/TrucoLive.spec.js
+++ cypress/integration/unit/TrucoLive.spec.js
@@ -6,9 +6,7 @@ window.customElements.define('truco-live', TrucoLive);
 describe('truco-live main component', () => {
   it('Component truco-live matchs input id', async () => {
     const id = 'test-id';
-    const componentUnderTest = await fixture(
-      html`<truco-live id="${id}"></truco-live>`
-    );
+    const componentUnderTest = await fixture(html`<truco-live id="${id}"></truco-live>`);
     expect(componentUnderTest.id).equal(id);
   });
 
